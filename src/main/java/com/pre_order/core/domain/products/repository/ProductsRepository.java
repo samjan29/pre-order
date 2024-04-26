74a76a24642b052c869812d8702660450b0b2d88
jan <samjan1127@gmail.com>
2024-04-26 16:22:31 +0900

    :sparkles: feat: 상품 상세 조회 #12

--- src/main/java/com/pre_order/core/domain/products/repository/ProductsRepository.java
+++ src/main/java/com/pre_order/core/domain/products/repository/ProductsRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductsRepository extends JpaRepository<Products, Long> {
+import java.util.Optional;
 
+public interface ProductsRepository extends JpaRepository<Products, Long> {
     Page<ProductResponseDto> findAllByIsShow(Pageable pageable, Boolean isShow);
+    Optional<ProductResponseDto> findByIdAndIsShow(Long id, Boolean isShow);
 }
