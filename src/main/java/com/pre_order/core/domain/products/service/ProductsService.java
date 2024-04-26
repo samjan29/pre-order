9547a2db739caa943202f846bea568fbeee2d458
jan <samjan1127@gmail.com>
2024-04-26 16:22:31 +0900

    :sparkles: feat: 상품 상세 조회 #12

--- src/main/java/com/pre_order/core/domain/products/service/ProductsService.java
+++ src/main/java/com/pre_order/core/domain/products/service/ProductsService.java
@@ -2,6 +2,8 @@ package com.pre_order.core.domain.products.service;
 
 import com.pre_order.core.domain.products.dto.ProductResponseDto;
 import com.pre_order.core.domain.products.repository.ProductsRepository;
+import com.pre_order.core.global.exception.CustomException;
+import com.pre_order.core.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,4 +25,8 @@ public class ProductsService {
         return productsRepository.findAllByIsShow(pageable, true).getContent();
     }
 
+    public ProductResponseDto getProduct(Long productId) {
+        return productsRepository.findByIdAndIsShow(productId, true)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+    }
 }
