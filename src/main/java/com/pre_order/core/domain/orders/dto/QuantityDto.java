01dd81165fa0e5409186b2d302bbfb654f97364c
jan <samjan1127@gmail.com>
2024-04-29 10:20:52 +0900

    :sparkles: feat: WishList 상품 변경 #19

---
+++ src/main/java/com/pre_order/core/domain/orders/dto/QuantityDto.java
@@ -0,0 +1,4 @@
+package com.pre_order.core.domain.orders.dto;
+
+public record QuantityDto(Integer quantity) {
+}
--- src/main/java/com/pre_order/core/domain/orders/service/OrdersService.java
+++ src/main/java/com/pre_order/core/domain/orders/service/OrdersService.java
@@ -1,13 +1,17 @@
 package com.pre_order.core.domain.orders.service;
 
+import com.pre_order.core.domain.orders.dto.QuantityDto;
 import com.pre_order.core.domain.orders.dto.WishListRequestDto;
 import com.pre_order.core.domain.orders.entity.WishList;
 import com.pre_order.core.domain.orders.repository.OrdersRepository;
 import com.pre_order.core.domain.orders.repository.WishListRepository;
 import com.pre_order.core.domain.products.entity.Products;
 import com.pre_order.core.domain.users.entity.Users;
+import com.pre_order.core.global.exception.CustomException;
+import com.pre_order.core.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +26,13 @@ public class OrdersService {
                 .users(user)
                 .quantity(wishListRequestDto.quantity())
                 .build());
+    }
+
+    @Transactional
+    public QuantityDto updateWishList(Long wishListId, WishListRequestDto wishListRequestDto) {
+        WishList wishList = wishListRepository.findById(wishListId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+        wishList.updateQuantity(wishListRequestDto.quantity());
+        return new QuantityDto(wishList.getQuantity());
     }
 }
