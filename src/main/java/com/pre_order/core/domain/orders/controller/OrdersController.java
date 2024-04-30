5c52ae751a0a67cc980d01edf131733c62cd7583
jan <samjan1127@gmail.com>
2024-04-30 13:50:04 +0900

    :recycle: refactor: 주문 도메인 나누기

--- src/main/java/com/pre_order/core/domain/orders/controller/OrdersController.java
+++ src/main/java/com/pre_order/core/domain/orders/controller/OrdersController.java
@@ -1,8 +1,6 @@
 package com.pre_order.core.domain.orders.controller;
 
 import com.pre_order.core.domain.orders.dto.QuantityDto;
-import com.pre_order.core.domain.orders.dto.WishListRequestDto;
-import com.pre_order.core.domain.orders.dto.WishListResponseDto;
 import com.pre_order.core.domain.orders.service.OrdersService;
 import com.pre_order.core.domain.products.entity.Products;
 import com.pre_order.core.domain.products.service.ProductsService;
@@ -14,8 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
@@ -24,21 +20,14 @@ public class OrdersController {
     private final ProductsService productsService;
     private final OrdersService ordersService;
 
-    @PostMapping("/wish-list")
-    public ResponseEntity<Void> addToWishList(@Valid @RequestBody WishListRequestDto wishListRequestDto, @AuthenticationPrincipal Users user) {
-        Products product = productsService.checkProduct(wishListRequestDto);
-        ordersService.addToWishList(wishListRequestDto, user, product);
+    @PostMapping("/{productId}")
+    public ResponseEntity<Void> createOrder(
+            @PathVariable(name = "productId") Long productId,
+            @Valid @RequestBody QuantityDto quantityDto,
+            @AuthenticationPrincipal Users user) {
+        Products product = productsService.checkProduct(productId);
+        ordersService.createOrder(product, quantityDto, user);
+        productsService.updateStock(quantityDto.quantity(), product);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
-    @GetMapping("/wish-list")
-    public ResponseEntity<List<WishListResponseDto>> getWishList(@AuthenticationPrincipal Users user) {
-        return ResponseEntity.ok().body(ordersService.getWishList(user));
-    }
-
-    @PatchMapping("/wish-list/{wishListId}")
-    public ResponseEntity<QuantityDto> updateWishList(@PathVariable(name = "wishListId") Long wishListId, @Valid @RequestBody WishListRequestDto wishListRequestDto) {
-        return ResponseEntity.ok().body(ordersService.updateWishList(wishListId, wishListRequestDto));
-    }
-
 }
---
+++ src/main/java/com/pre_order/core/domain/orders/service/WishListService.java
@@ -0,0 +1,45 @@
+package com.pre_order.core.domain.orders.service;
+
+import com.pre_order.core.domain.orders.dto.QuantityDto;
+import com.pre_order.core.domain.orders.dto.WishListRequestDto;
+import com.pre_order.core.domain.orders.dto.WishListResponseDto;
+import com.pre_order.core.domain.orders.entity.WishList;
+import com.pre_order.core.domain.orders.repository.WishListRepository;
+import com.pre_order.core.domain.products.entity.Products;
+import com.pre_order.core.domain.users.entity.Users;
+import com.pre_order.core.global.exception.CustomException;
+import com.pre_order.core.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class WishListService {
+
+    private final WishListRepository wishListRepository;
+
+    public void addToWishList(WishListRequestDto wishListRequestDto, Users user, Products product) {
+        wishListRepository.save(WishList.builder()
+                .products(product)
+                .users(user)
+                .quantity(wishListRequestDto.quantity())
+                .build());
+    }
+
+    public List<WishListResponseDto> getWishList(Users user) {
+        // TODO JPQL로 변경
+        List<WishList> wishListList = wishListRepository.findAllByUsersIdAndIsDeleted(user.getId(), false);
+        return WishListResponseDto.listFrom(wishListList);
+    }
+
+    @Transactional
+    public QuantityDto updateWishList(Long wishListId, WishListRequestDto wishListRequestDto) {
+        WishList wishList = wishListRepository.findById(wishListId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND));
+        wishList.updateQuantity(wishListRequestDto.quantity());
+        return new QuantityDto(wishList.getQuantity());
+    }
+}
